447135529fbca656a4a2e9da8a66600c913148c0
vishal singh <vishalkumarmps@gmail.com>
2022-05-11 14:34:54 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,184 @@
-import logo from './logo.svg';
-import './App.css';
+import firebase from "firebase/compat/app";
+import React from "react";
+import Cart from "./Cart";
+import Navbar from "./Navbar";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(){
+      super();
+      this.state = {
+          products:[],
+          loading: true
+          
+      }
+      const db = firebase.firestore();
+  }
+  // read data form the data base
+  componentDidMount(){
+    // firebase
+    //   .firestore()
+    //   .collection('products')
+    //   .get()
+    //   .then((snapshot)=>{
+    //     console.log(snapshot);
+    //     snapshot.docs.map((doc)=>{
+    //       console.log(doc.data());
+    //     });
+    //     const products = snapshot.docs.map((doc)=>{
+    //       const data = doc.data();
+    //       data['id'] = doc.id;
+    //       return data;
+    //     });
+    //     console.log(products);
+    //     this.setState({
+    //       products,
+    //       loading: false
+    //     })
+    //   });
+
+    firebase
+    .firestore()
+    .collection('products')
+    .onSnapshot((snapshot)=>{
+      console.log(snapshot);
+      snapshot.docs.map((doc)=>{
+        console.log(doc.data());
+      });
+      const products = snapshot.docs.map((doc)=>{
+        const data = doc.data();
+        data['id'] = doc.id;
+        return data;
+      });
+
+      console.log(products);
+      this.setState({
+        products,
+        loading: false
+      })
+    });
+
+    
+  }
+  handleIncreaseQuantity = (product)=>{
+      const {products} = this.state;
+      const index = products.indexOf(product);
+      // console.log();
+      const docRef = firebase.firestore().collection('products').doc(products[index].id);
+
+      docRef
+      .update({
+        qty: products[index].qty + 1
+      })
+      .then(()=>{
+        console.log('Updated Successfully');
+      })
+      .catch((err)=>{
+        console.log('Error ', err);
+      })
+      // products[index].qty += 1;
+      // this.setState({
+      //     products:products
+      //     // or
+      //     // products
+      // })
+  }
+  handleDecreaseQuantity = (product)=>{
+      const {products} = this.state;
+      const index = products.indexOf(product);
+
+      const docRef = firebase.firestore().collection('products').doc(products[index].id);
+
+      docRef
+      .update({
+        qty: products[index].qty - 1
+      })
+      .then(()=>{
+        console.log('Updated Successfully');
+      })
+      .catch((err)=>{
+        console.log('Error ', err);
+      })
+
+      // products[index].qty -= 1;
+      // this.setState({
+      //     products
+      // })
+  }
+  handleDeleteQuantity = (id)=>{
+      const {products} = this.state;
+      
+      const docRef = firebase.firestore().collection('products').doc(id);
+
+      docRef
+      .delete()
+      .then(()=>{
+        console.log('delete Successfully');
+      })
+      .catch((err)=>{
+        console.log('Error ', err);
+      })
+
+
+      // const items = products.filter((item)=>item.id !== id);
+      // this.setState({
+      //     products:items
+      // })
+  }
+  getCartCount = ()=>{
+    const {products} = this.state;
+
+    let count = 0;
+    products.forEach((product)=>{
+      count +=product.qty;
+    })
+    return count;
+  }
+  getTotalPrice =()=>{
+    const {products} = this.state;
+    let price = 0;
+    products.forEach((product)=>{
+      price += product.price;
+    })
+    return price;
+  }
+  // add product to the firebase 
+  addProduct = ()=>{
+    this.db
+    .collection('products')
+    .add({
+      img:"https://image.shutterstock.com/image-photo/modern-computer-mouse-on-white-260nw-1090541303.jpg",
+      price:600,
+      qty:1,
+      title:"Mouse"
+    })
+    .then((docRef)=>{
+      console.log('Product has been added ', docRef);
+    })
+    .catch((err)=>{
+      console.log('Error', err);
+    })
+  }
+  render(){  
+    const { products, loading } = this.state;
+    return (
+      <div className="App">
+      <Navbar
+        count ={this.getCartCount()}
+      />
+      <button onClick={this.addProduct} style={{padding:20, font:20, backgroundColor:"blue"}}>Add Product</button>
+      <Cart
+        products={products}
+        onIncreaseQuantity = {this.handleIncreaseQuantity}
+        onDecreaseQuantity = {this.handleDecreaseQuantity}
+        onDeleteQuantity = {this.handleDeleteQuantity}
+      />
+      {loading && <h1>Loading products ...</h1>}
+      <div style={{fontSize: 20, padding:10}}>
+        TOTAL: {this.getTotalPrice()}
+      </div>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,29 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+// import * as serviceWorker from 'serviceWorker';
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/firestore';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBH4r65uRi1lUjm4Dc5EkVE5--wPeQMRJU",
+  authDomain: "cart-b3ed6.firebaseapp.com",
+  projectId: "cart-b3ed6",
+  storageBucket: "cart-b3ed6.appspot.com",
+  messagingSenderId: "294140013831",
+  appId: "1:294140013831:web:a9d8b473079008a84669a2"
+};
+firebase.initializeApp(firebaseConfig);
+ReactDOM.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
